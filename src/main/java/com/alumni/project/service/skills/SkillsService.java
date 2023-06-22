593cd65cd43f3f610323d63856fc821049b250e0
kristalinaa <kristalinaveliu7@gmail.com>
2023-06-22 10:03:30 +0100

    skills

--- src/main/java/com/alumni/project/service/skills/SkillsService.java
+++ src/main/java/com/alumni/project/service/skills/SkillsService.java
@@ -1,4 +1,15 @@
 package com.alumni.project.service.skills;
 
+import com.alumni.project.dal.entity.Skills;
+import com.alumni.project.dal.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface SkillsService {
+    Skills save(Skills skills);
+    List<Skills> findAll();
+    Skills findById(UUID id);
+    Skills update(Long id, Skills dto);
+    void delete(UUID id);
 }
--- src/main/java/com/alumni/project/service/skills/SkillsServiceImpl.java
+++ src/main/java/com/alumni/project/service/skills/SkillsServiceImpl.java
@@ -1,9 +1,60 @@
 package com.alumni.project.service.skills;
 
+import com.alumni.project.dal.entity.Skills;
+import com.alumni.project.dal.repository.SkillsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SkillsServiceImpl implements SkillsService{
+    private final SkillsRepository skillsRepository;
+
+
+    @Override
+    public Skills save(Skills skills) {
+            var skill = new Skills(
+                    Skills.getSkillField(),
+                    Skills.getSkillDescription()
+
+            );
+
+            var saved = skillsRepository.save(skill);
+            return map(saved);
+
+    }
+
+    @Override
+    public List<Skills> findAll() {
+
+        return skillsRepository.findAll()
+                .stream()
+                .map(this::map)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Skills findById(UUID id) {
+
+        var optional = skillsRepository.findById(id);
+        if (optional.isPresent()) {
+            return map(optional.get());
+        }
+        throw new RuntimeException("User not found");
+    }
+
+    @Override
+    public Skills update(UUID id, Skills dto) {
+
+        return null;
+    }
+
+    @Override
+    public void delete(UUID id) {
+
+    }
 }
